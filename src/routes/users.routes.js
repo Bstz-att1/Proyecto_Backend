f7c8e971f2a0bc9b2410e189470d46e87d74a7e0
Bstz-att1 <hfdarioalexander7@gmail.com>
2026-04-11 19:34:41 -0500

    refactor(users): integrar validación con Zod y middleware en controladores y rutas

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,6 +7,8 @@ import {
   patchUserById,
   deleteUserById
 } from '../controllers/users.controller.js';
+import { validateSchema } from '../middlewares/validator.middleware.js';
+import { userSchema } from '../schemas/users.schema.js';
 
 const router = Router();
 
@@ -17,13 +19,13 @@ router.get('/', getUsers);
 router.get('/:id', getUserById);
 
 // Registrar un nuevo usuario
-router.post('/', createUser);
+router.post('/',  validateSchema(userSchema), createUser);
 
 // Actualizar información de un usuario
-router.put('/:id', updateUserById);
+router.put('/:id', validateSchema(userSchema),  updateUserById);
 
 // Actualizar informacion de un usuario parcialmente
-router.patch('/:id', patchUserById);
+router.patch('/:id', validateSchema(userSchema.partial()),  patchUserById);
 
 // Eliminar un usuario
 router.delete('/:id', deleteUserById);
